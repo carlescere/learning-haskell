602c585cacd177a98e662341b8fa25bc637134a4
Carles Cerezo Guzmán <carlescere@gmail.com>
2016-10-11 21:40:22 +0100

    Add guards to tree implementation

--- tree/tree.hs
+++ tree/tree.hs
@@ -2,7 +2,9 @@ data BinTree a = Nil | Node (BinTree a) a (BinTree a) deriving Show
 
 insert :: Ord a => a -> BinTree a -> BinTree a
 insert x Nil = Node Nil x Nil
-insert x (Node l v r) = if x > v then Node l v (insert x r) else Node (insert x l) v r
+insert x (Node l v r)
+  | x > v     = Node l v (insert x r)
+  | otherwise = Node (insert x l) v r
 
 isEmpty :: Ord a => BinTree a -> Bool
 isEmpty Nil = True
@@ -14,7 +16,10 @@ isLeaf _ = False
 
 exist :: Ord a => a -> BinTree a -> Bool
 exist _ Nil = False
-exist x (Node l v r) = x == v || if x > v then exist x r else exist x l
+exist x (Node l v r) = x == v || exist x dir
+  where dir
+          | x > v     = r
+          | otherwise = l
 
 toArr :: Ord a => BinTree a -> [a]
 toArr Nil = []
